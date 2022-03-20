d6d596d14ed8ee0774c8eb1a1f18b0388bf6b1ea
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-03-20 07:59:07 +0100

    ignore dev files

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+const express = require('express');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(cors());
+app.get('/', async(req, res) => {
+    getUserIp(req.ip, '/');
+    return res.status(200).render('index.ejs');
+});
+app.listen(PORT, () => console.log(`Server listening for request on ${PORT}`));
